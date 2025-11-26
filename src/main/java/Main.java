2df372421ecaffbfb0878274922b1170dc004b15
jullierodriguez <jullisa.rodriguez4@gmail.com>
2025-11-26 09:49:44 -0800

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My fun fact:");
-        System.out.println("TODO: replace this text in a Git branch.");
+        System.out.println("TODO: I have dyed my hair different shades of blue and purple throughout the years");
     }
 }
