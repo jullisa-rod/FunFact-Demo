6a6c427d5dfe3c3dd9a9bdedb80677527ab2c0a7
jullierodriguez <jullisa.rodriguez4@gmail.com>
2025-11-26 10:23:35 -0800

    added bucketlist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ public class Main {
         System.out.println("My fun fact:");
         System.out.println("TODO: I have dyed my hair different shades of blue and purple throughout the years");
         System.out.println("\nMy bucketlist item:");
-        System.out.println("I want to travel to different parts of Mexico (Oaxaca and Mexico City) as well as Korea and Japan"
-        + " I also want to live in a different country for a couple of years, (i don't have a specific place in mind yet)");
+        System.out.println("I want to travel to different parts of Mexico (Oaxaca and Mexico City) as well as Korea and Japan");
+        System.out.println(" I also want to live in a different country for a couple of years, (i don't have a specific place in mind yet)");
 
     }
 }
